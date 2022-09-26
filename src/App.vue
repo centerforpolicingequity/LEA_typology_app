db33aa421a54c2c2ac6365c697bbd3103b3dc660
Ari <mabramovaa01@gmail.com>
2022-09-26 13:33:39 -0400

    fix

---
+++ src/App.vue
@@ -0,0 +1,127 @@
+<template>
+  <header>
+    <img src="./components/cpe_logo.png" alt="cpe_logo">
+  <h2>LEA Typology</h2>
+  <div class="user-data">
+        <p> Name: Test User </p>
+        <p> Role: TestUser </p>
+      </div>
+  </header>
+  <p>Click below to explore the database.</p>
+  <div class="main-app-panel">
+  <div class="menu-panel">
+    <ul>
+      <li><button @click="type = 'data'">Data View</button></li>
+      <li><button @click="type = 'variable'">Variable View</button></li>
+      <li><button @click="type = 'agency'">Agency View</button></li>
+      <li><button @click="type = 'city'">City View</button></li>
+      <li><button @click="type = 'state'">State View</button></li>
+      <li><button @click="type = 'country'">Country View</button></li>
+    </ul>
+  </div>
+  <div class="display-panel">
+    <DataView v-if="type === 'data'"></DataView>
+    <VariableView v-else-if="type === 'variable'"></VariableView>
+    <AgencyView v-else-if="type === 'agency'"></AgencyView>
+    <CityView v-else-if="type === 'city'"></CityView>
+    <StateView v-else-if="type === 'state'"></StateView>
+    <CountryView v-else-if="type === 'country'"></CountryView>
+  </div>
+</div>
+<footer>
+  <p>...</p>
+</footer>
+</template>
+
+<script>
+
+import DataView from './components/DataView.vue';
+import VariableView from './components/VariableView.vue';
+import AgencyView from './components/AgencyView.vue';
+import CityView from './components/CityView.vue';
+import StateView from './components/StateView.vue';
+import CountryView from './components/CountryView.vue';
+
+export default {
+  name: 'App',
+  props: {
+  },
+  data() {
+    return {
+      type: 'data'
+    }
+  },
+  components: {
+    DataView,
+    VariableView,
+    AgencyView,
+    CityView,
+    StateView,
+    CountryView
+  },
+  methods: {
+  },
+  mounted() {
+    console.log("mounted")
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  padding: 2%;
+}
+button{
+  cursor: pointer!important;
+}
+header{
+  display: block;
+  width: 100%;
+  height: 8vh;
+  align-items: flex-start;
+}
+header img{
+  height: 100%;
+  float: left;
+}
+header h2{
+  float: left;
+  padding-left: 2.5%;
+}
+.user-data{
+  float: right;
+}
+.main-app-panel{
+  display: flex;
+  width: 100%;
+  height: 100%;
+}
+.menu-panel {
+  background-color: lightblue;
+  width: 15%;
+}
+
+.menu-panel ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
+
+.menu-panel button {
+  width: 100%;
+  text-align: left;
+  cursor: pointer;
+  font-size: large;
+}
+
+.display-panel {
+  background-color: lightgray;
+  width: 100%;
+  height: 80vh;
+}
+</style>
---
+++ src/components/DataView.vue
@@ -0,0 +1,166 @@
+<template>
+  <h1>Data View</h1>
+  <hr>
+  <div class="settings">
+    <div class="col-1">
+      <div class="dropdown">
+        <button @click="this.myFunction1()" class="dropbtn">Data Group</button>
+        <div id="myDropdown1" class="dropdown-content">
+          <a href="#">Link 1</a>
+          <a href="#">Link 2</a>
+          <a href="#">Link 3</a>
+        </div>
+      </div>
+      <div class="dropdown">
+        <button @click="this.myFunction2()" class="dropbtn">Data Set</button>
+        <div id="myDropdown2" class="dropdown-content">
+          <a href="#">Link 1</a>
+          <a href="#">Link 2</a>
+          <a href="#">Link 3</a>
+        </div>
+      </div>
+
+    </div>
+    <div class="col-2">
+      <div class="extra-filters">
+        <p>Additional Filters</p>
+        <p>Time</p>
+        <div class="checkboxes">
+          <input type="checkbox" id="a" value="Bike">
+          <label for="a"> Filter by date </label>
+          <input type="checkbox" id="b" value="Bike">
+          <label for="b"> Display SQL </label>
+          <input type="checkbox" id="c" value="Bike">
+          <label for="c"> Include Sorting </label>
+          <input type="checkbox" id="d" value="Bike">
+          <label for="d"> Limit Results </label>
+        </div>
+        <button>Run Query</button>
+      </div>
+    </div>
+  </div>
+  <div class="data-table">
+    <div class="table-buttons">
+    <button>Raw</button>
+    <button>By State</button>
+    <button>By Country</button>
+    <button>By Type</button>
+  </div>
+  <br/>
+    <table>
+      <tr>
+        <th>Index</th>
+        <th>lear_id</th>
+        <th>agencyname</th>
+      </tr>
+    </table>
+    <img src="./datatable.png" alt="data table">
+    <button>Export</button>
+    <div class="SQL query">
+      <p>(Shows SQL Query)</p>
+    </div>
+  </div>
+</template>
+  
+<script>
+export default {
+  name: 'DataView',
+  props: {
+  },
+  methods: {
+    myFunction1() {
+      document.getElementById("myDropdown1").classList.toggle("show");
+    },
+    myFunction2() {
+      document.getElementById("myDropdown2").classList.toggle("show");
+    }
+  }
+}
+</script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+.settings {
+  height: 40%;
+  display: flex;
+}
+
+.col-1 {
+  float: left;
+  flex: 40%;
+  border-right-style: solid;
+  border-right-color: darkgray;
+  margin-right: 1.5%;
+}
+
+.col-2 {
+  float: left;
+  flex: 60%;
+}
+
+/* Dropdown Button */
+.dropbtn {
+  cursor: pointer;
+}
+
+.dropdown {
+  float: left;
+  position: relative;
+  display: inline-block;
+}
+
+/* Dropdown Content (Hidden by Default) */
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #f1f1f1;
+  z-index: 1;
+  width: 100%;
+}
+
+/* Links inside the dropdown */
+.dropdown-content a {
+  color: black;
+  display: block;
+}
+
+.show {
+  display: block;
+}
+.col-2{
+  text-align: left;
+}
+.col-2 .user-data {
+  display: flex;
+}
+.extra-settings {
+  background-color: azure;
+}
+
+.extra-settings .checkboxes {
+  display: flex;
+  align-content: flex-start;
+}
+
+.data-table {
+  padding: 1.5%;
+  height: fit-content;
+  background-color: lightcyan;
+  overflow: hidden;
+}
+.data-table button{
+  float: left;
+}
+.table-buttons{
+  float: left;
+}
+table{
+  background-color: azure;
+  width: 100%;
+  height: 20vh;
+}
+.data-table img{
+  width: 100%;
+}
+</style>
+  
